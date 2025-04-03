41d244ca0fec475964600c179be767dd6543cfcb
MitsoaAllan <hei.mitsoa@gmail.com>
2025-04-03 13:52:01 +0300

    feat: Refactor database structure

--- migration/2_create_table_events.sql
+++ migration/2_create_table_events.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS events (
     id BIGSERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
     location VARCHAR(255) NOT NULL,
-    event_date DATE NOT NULL,
+    date DATE NOT NULL,
     time TIME NOT NULL,
     organizer_id INT REFERENCES users(id) ON DELETE SET NULL,
     standard_price numeric NOT NULL,
@@ -12,7 +13,7 @@ CREATE TABLE IF NOT EXISTS events (
     early_bird_price numeric NOT NULL,
     vip_quantity INT NOT NULL,
     image_url VARCHAR NOT NULL,
-    category VARCHAR DEFAULT 'Recent',
+    category VARCHAR DEFAULT 'recent',
     type VARCHAR NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
